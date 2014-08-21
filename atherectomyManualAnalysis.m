29e2f76bf02912e5be275c21945bf66f911f84b0
Barry Belmont <belmont@umich.edu>
2014-08-21 15:08:03 -0400

    Stable-ish

--- atherectomyManualAnalysis.m
+++ atherectomyManualAnalysis.m
@@ -16,3 +16,37 @@ frameOfInterest = 165;
 videoFile = VideoReader(videoInfo.filename);
 imageOfInterest = read(videoFile,frameOfInterest);
 imtool(imageOfInterest)
+
+
+%%
+
+[nRow, nCols, nFrames] = size(videoROI);
+
+
+level = graythresh(videoROI(:,:,1));
+
+for indFrames = 1:200%nFrames
+    BW(:,:,indFrames) = im2bw(videoROI(:,:,indFrames),level);
+    for indCol = 1:nCols
+        x = find(BW(:,indCol,indFrames),1,'last');
+        if size(x,1) == 1
+            bottomEdge(indCol,indFrames) = x;
+        elseif size(x,1) == 0
+            bottomEdge(indCol,indFrames) = NaN;
+        end
+%         bottomEdgeSMOOTH(indCol,indFrames) = smooth(bottomEdge(indCol,indFrames),10);
+    
+    end
+    displacement(:,indFrames) = bottomEdge(:,indFrames) ...
+        - bottomEdge(:,1);
+    displacementSMOOTH(:,indFrames) = smooth(bottomEdge(:,indFrames) ...
+        - bottomEdge(:,1),10);
+end
+
+for indCol = 1:nCols
+    
+end
+    
+
+    framesToAnalyze = 200;
+mesh(displacementSMOOTH(:,1:framesToAnalyze))
